fa6afb8b6b4a48c3160e90e50dad33355ab8857d
kodai kina <e165724@ie.u-ryukyu.ac.jp>
2017-01-17 08:54:26 +0900

    tikuwa

---
+++ src/test/jp/ac/uryukyu/ie/e165724/EnemyTest.java
@@ -0,0 +1,23 @@
+package jp.ac.uryukyu.ie.e165724;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165718 on 2016/11/22.
+ */
+public class EnemyTest {
+    @Test
+    public void attack() throws Exception {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.setDead(true);
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.getHitPoint());
+    }
+
+}
